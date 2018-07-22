5ac92679a7b0ec2fd07f62082bc24d03f833925b
akakura <akakura3@gmail.com>
2018-07-22 09:04:44 +0900

    start learning OSbook ref: http://kozos.jp/books/makeos

---
+++ 01/bootload/serial.h
@@ -0,0 +1,8 @@
+#ifndef _SERIAL_H_INCLUDED
+#define _SERIAL_H_INCLUDED
+
+int serial_init(int index);
+int serial_is_send_enavble(int index);
+int serial_send_byte(int index, unsigned char b);
+
+#endif
